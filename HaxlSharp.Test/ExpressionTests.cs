51ff2c93e52a82c79eaeba25da1f12d87867b2a7
joashc <joash.is@gmail.com>
2016-06-09 19:17:34 +1000

    Switch to using FetchResult

--- HaxlSharp.Test/ExpressionTests.cs
+++ HaxlSharp.Test/ExpressionTests.cs
@@ -3,30 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using HaxlSharp.Internal;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HaxlSharp.Test
 {
     public class Nested
     {
-        public int x { get { return 99; } }
-    }
-
-    public class TestFetch<A> : FetchNode<A>
-    {
-        public readonly A Value;
-
-        public TestFetch(A val)
-        {
-            Value = val;
-        }
-
-        public override Haxl ToHaxlFetch(string bindTo, Scope scope)
-        {
-            return Haxl.Pure(bindTo, Value);
-        }
+        public int x => 99;
     }
 
 
@@ -34,11 +16,11 @@ namespace HaxlSharp.Test
     public class ExpressionSplitTests
     {
         public const int global = 2;
-        public static TestFetch<int> a = new TestFetch<int>(global);
-        public static TestFetch<int> b = new TestFetch<int>(3);
-        public static Func<int, TestFetch<int>> c = i => new TestFetch<int>(i);
-        public static Func<int, int, TestFetch<int>> c2 = (i, i2) => new TestFetch<int>(i);
-        public static Func<int, TestFetch<int>> d = i => new TestFetch<int>(i);
+        public static FetchResult<int> a = new FetchResult<int>(global);
+        public static FetchResult<int> b = new FetchResult<int>(3);
+        public static Func<int, FetchResult<int>> c = i => new FetchResult<int>(i);
+        public static Func<int, int, FetchResult<int>> c2 = (i, i2) => new FetchResult<int>(i);
+        public static Func<int, FetchResult<int>> d = i => new FetchResult<int>(i);
         public static Nested nested = new Nested();
 
         public static int CountAt(List<ApplicativeGroup> split, int i)
@@ -68,20 +50,20 @@ namespace HaxlSharp.Test
         {
             // We've got two variables of different type named 'x'.
 
-            var nested =     from x in new TestFetch<string>("1")           // Group 0.1
-                             // split                                       // =========
-                             from za in c(int.Parse(x))                     // Group 1.1
-                             from ya in b                                   // Group 1.2
-                             //projection                                   // =========
-                             select ya;                                     // Group 2.1 (Projection)
-            var expression = from x in nested                               // 
-                             // split                                 		// =========
-                             from z in c(x)                           		// Group 3.1
-                             from y in b                              		// Group 3.2
-                             // split                                 		// =========
-                             from w in d(y)                           		// Group 4.1
-                             // projection                            		// =========
-                             select x + y + z + w;                    		// Group 5.1 (Projection)
+            var nested =     from x in new FetchResult<string>("1")           // Group 0.1
+                             // split                                         // =========
+                             from za in c(int.Parse(x))                       // Group 1.1
+                             from ya in b                                     // Group 1.2
+                             //projection                                     // =========
+                             select ya;                                       // Group 2.1 (Projection)
+            var expression = from x in nested                                 // 
+                             // split                                 		  // =========
+                             from z in c(x)                           		  // Group 3.1
+                             from y in b                              		  // Group 3.2
+                             // split                                 		  // =========
+                             from w in d(y)                           		  // Group 4.1
+                             // projection                            		  // =========
+                             select x + y + z + w;                    		  // Group 5.1 (Projection)
 
             var split = Split(expression);
             Assert.AreEqual(4, SplitCount(split));
@@ -112,20 +94,20 @@ namespace HaxlSharp.Test
         [TestMethod]
         public void ExpressionTest()
         {
-            var nested =     from xa in new TestFetch<int>(66)        // Group 0.1
-                             // split                                 // =========
-                             from za in c(xa)                         // Group 1.1
-                             from ya in b                             // Group 1.2
-                             //projection                             // =========
-                             select xa + ya + za;                     // Group 2.1 (Projection)
-            var expression = from x in nested                         // 
-                             // split                                 // =========
-                             from z in c(x)                           // Group 3.1
-                             from y in b                              // Group 3.2
-                             // split                                 // =========
-                             from w in d(y)                           // Group 4.1
-                             // projection                            // =========
-                             select x + y + z + w;                    // Group 5.1 (Projection)
+            var nested =     from xa in new FetchResult<int>(66)        // Group 0.1
+                             // split                                   // =========
+                             from za in c(xa)                           // Group 1.1
+                             from ya in b                               // Group 1.2
+                             //projection                               // =========
+                             select xa + ya + za;                       // Group 2.1 (Projection)
+            var expression = from x in nested                           // 
+                             // split                                   // =========
+                             from z in c(x)                             // Group 3.1
+                             from y in b                                // Group 3.2
+                             // split                                   // =========
+                             from w in d(y)                             // Group 4.1
+                             // projection                              // =========
+                             select x + y + z + w;                      // Group 5.1 (Projection)
 
             var split = Split(expression);
             Assert.AreEqual(4, SplitCount(split));
@@ -141,7 +123,7 @@ namespace HaxlSharp.Test
         public void ExpressionTest2()
         {
             var expression = from x in a
-                             from y in new TestFetch<int>(nested.x)
+                             from y in new FetchResult<int>(nested.x)
                                  // split
                              from z in c2(x, y)
                              select x + y + z;
@@ -238,10 +220,10 @@ namespace HaxlSharp.Test
         public void SequenceRewrite()
         {
             var list = Enumerable.Range(0, 10);
-            Func<int, TestFetch<int>> mult10 = x => new TestFetch<int>(x * 10);
-            var expression = from x in new TestFetch<IEnumerable<int>>(list)
+            Func<int, FetchResult<int>> mult10 = x => new FetchResult<int>(x * 10);
+            var expression = from x in new FetchResult<IEnumerable<int>>(list)
                              from multiplied in x.SelectFetch(mult10)
-                             from added in x.SelectFetch(num => new TestFetch<int>(num + 1))
+                             from added in x.SelectFetch(num => new FetchResult<int>(num + 1))
                              select added.Concat(multiplied);
 
             var split = Split(expression);
@@ -256,10 +238,10 @@ namespace HaxlSharp.Test
         public void SequenceRewriteConcurrent()
         {
             var list = Enumerable.Range(0, 1000);
-            Func<int, TestFetch<int>> mult10 = x => new TestFetch<int>(x * 10);
-            var expression = from x in new TestFetch<IEnumerable<int>>(list)
+            Func<int, FetchResult<int>> mult10 = x => new FetchResult<int>(x * 10);
+            var expression = from x in new FetchResult<IEnumerable<int>>(list)
                              from multiplied in x.SelectFetch(mult10)
-                             from added in x.SelectFetch(num => new TestFetch<int>(num))
+                             from added in x.SelectFetch(num => new FetchResult<int>(num))
                              select added.Concat(multiplied);
 
             var split = Split(expression);
@@ -272,18 +254,14 @@ namespace HaxlSharp.Test
         [TestMethod]
         public void OneLiner()
         {
-            var oneLine = from x in new TestFetch<int>(3)
+            var oneLine = from x in new FetchResult<int>(3)
                           select x + 1;
-            var split = Split(oneLine);
-            Assert.AreEqual(1, SplitCount(split));
-            Assert.AreEqual(2, CountAt(split, 0));
-            Assert.AreEqual(1, CountAt(split, 1));
         }
 
         [TestMethod]
         public void SelectTest()
         {
-            var number = new TestFetch<int>(3);
+            var number = new FetchResult<int>(3);
             var plusOne = number.Select(num => num + 1);
             var plusTwo = plusOne.Select(num => num + 1);
         }
