afe45048da31f7b7caaccd9157e6633be9d5abb1
Courtney Strachan <courtney.strachan@gmail.com>
2016-06-21 16:57:29 -0700

    Updated the HaxlSharp.Fetcher project to use the BouncyCastle nuget package for crypto functions.

--- HaxlSharp.Fetcher/HashedRequestKey.cs
+++ HaxlSharp.Fetcher/HashedRequestKey.cs
@@ -1,6 +1,7 @@
 ﻿using static HaxlSharp.Internal.Base;
 using Newtonsoft.Json;
-using xBrainLab.Security.Cryptography;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Utilities.Encoders;
 
 namespace HaxlSharp
 {
@@ -11,10 +12,23 @@ namespace HaxlSharp
             return StaticForRequest(request);
         }
 
+        private static byte[] Md5Hash(byte[] input)
+        {
+            // Update the input of MD5
+            var md5 = new MD5Digest();
+            md5.BlockUpdate(input, 0, input.Length);
+
+            // Get the output and hash it
+            var output = new byte[md5.GetDigestSize()];
+            md5.DoFinal(output, 0);
+
+            return Hex.Encode(output);
+        }
+
         public static string StaticForRequest<A>(Returns<A> request)
         {
-            var json = JsonConvert.SerializeObject(request, request.GetType(), new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All, TypeNameAssemblyFormat= System.Runtime.Serialization.Formatters.FormatterAssemblyStyle.Full});
-            return compose(ToLowerHexString, MD5.GetHash, StringBytes)(json);
+            var json = JsonConvert.SerializeObject(request, request.GetType(), new JsonSerializerSettings {TypeNameHandling = TypeNameHandling.All, TypeNameAssemblyFormat = System.Runtime.Serialization.Formatters.FormatterAssemblyStyle.Full});
+            return compose(ToLowerHexString, Md5Hash, StringBytes)(json);
         }
     }
 }
